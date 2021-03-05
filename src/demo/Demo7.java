93196841ccfb568b3fdd011a8c3616a9a22ae1a8
zaizai <690969974@qq.com>
2021-03-05 16:02:13 +0800

    计算器

--- src/demo/Demo7.java
+++ src/demo/Demo7.java
@@ -1,7 +1,44 @@
 package demo;
 
-public class Demo7 {
-    public static void main(String[] args) {
-
+public class Demo7{
+    double a, c;
+    char b;
+    public Demo7(double a,char b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public String getanswer(){
+        if (b == '+'){
+            Double answer = add();
+            return answer.toString();
+        }
+        else if (b == '-'){
+            Double answer = subtraction();
+            return answer.toString();
+        }
+        else if (b == '*'){
+            Double answer = multiplication();
+            return answer.toString();
+        }
+        else if (b == '/'){
+            Double answer = division();
+            return answer.toString();
+        }
+        else {
+            return "xxxx";
+        }
+    }
+    public double add(){
+        return a + c;
+    }
+    public double subtraction(){
+        return a - c;
+    }
+    public double multiplication(){
+        return a * c;
+    }
+    public double division(){
+        return a / c;
     }
 }
---
+++ src/demo/Demo8.java
@@ -0,0 +1,9 @@
+package demo;
+
+public class Demo8 {
+    public static void main(String[] args) {
+        Demo7 a = new Demo7(123, '-', 123.12);
+        String an = a.getanswer();
+        System.out.println(an);
+    }
+}
