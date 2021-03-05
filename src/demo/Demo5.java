2306f2595986224ea1994c1f4813ec225c819304
zaizai <690969974@qq.com>
2021-03-05 10:53:39 +0800

    几个demo

---
+++ src/demo/Demo5.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Demo5 {
+    public static void main(String args[]) {
+        int x = 10;
+        while (x < 20) {
+            System.out.println(String.format("value of x : %s", x));
+            x++;
+        }
+    }
+}
---
+++ src/demo/Demo6.java
@@ -0,0 +1,19 @@
+package demo;
+
+import java.util.Scanner;
+
+public class Demo6
+{
+    public static void main(String[] args) {
+        java.util.Scanner s = new java.util.Scanner(System.in);
+        System.out.print(": ");
+        int age = s.nextInt();
+        System.out.println("age = " + age);
+        if(age >= 10){
+            System.out.println("asdasdas");
+        }
+        else {
+            System.out.println("asd");
+        }
+    }
+}
